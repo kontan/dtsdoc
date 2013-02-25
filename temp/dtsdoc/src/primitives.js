5fb680e4f53d0fa2b3e81713a825d3c943bf5dbb
Kon <kudagitsune+git@gmail.com>
2013-02-25 12:24:46 +0900

    Ignore temp dir

--- temp/dtsdoc/src/primitives.js
+++
@@ -1,5 +0,0 @@
-var primitiveTypeNameLinks = {
-    "string": "http://phyzkit.net/docs/lib.d.ts.html#interface%20String",
-    "bool": "http://phyzkit.net/docs/lib.d.ts.html#interface%20Boolean",
-    "number": "http://phyzkit.net/docs/lib.d.ts.html#interface%20Number"
-};
